040c6a433af7b57c8d49188d56725ad4432cdee3
Jericho Sharman <jsharman@hotmail.com.au>
2024-05-28 22:25:47 +1000

    Remove unwanted directories and files from tracking and update .gitignore

--- PROJECTS/EVENTS_CALENDAR/php/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Start the session
-session_start();
-
-// Write some data to the session
-$_SESSION['write_test'] = 'Session write test successful';
-
-// Output the data
-echo $_SESSION['write_test'];
